bbf2974ed38a590db0b19b9a25be6fa6d0233f1e
ins72tt <tmonnens@outlook.com>
2024-05-29 07:33:14 +0700

    mongodb key

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://';
+dbPassword = 'mongodb+srv://freshrecc:PaUKmMhaIOJnkvyh@cluster0.tj7lpsc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 module.exports = {
     mongoURI: dbPassword
